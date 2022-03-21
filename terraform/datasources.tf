b4e631d6ff0c3f5a04b6491dda1a45c5958c9287
Deepak Devadathan <deepak.devadathan@oracle.com>
2022-03-21 20:01:26 +1100

    removed dependency

--- terraform/datasources.tf
+++ terraform/datasources.tf
@@ -27,5 +27,5 @@ data "oci_identity_availability_domains" "ADs" {
 
 data "oci_containerengine_cluster_kube_config" "oke" {
   cluster_id = oci_containerengine_cluster.oke_cluster.id
-  depends_on = [oci_containerengine_node_pool.oke_node_pool]
+  #depends_on = [oci_containerengine_node_pool.oke_node_pool]
 }
