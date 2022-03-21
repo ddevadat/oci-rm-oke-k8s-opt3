e9d829b88648ff255a720bf14262dcd862f07ada
Deepak Devadathan <deepak.devadathan@oracle.com>
2022-03-21 17:05:07 +1100

    changed provider path

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 1.0"
   required_providers {
     oci = {
-      source  = "oracle/oci"
+      source  = "hashicorp/oci"
       version = ">= 4.36.0"
     }
     kubernetes = {
